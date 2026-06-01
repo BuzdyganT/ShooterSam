34db801b8181a5d870b09c6d02a7e2e8924d4de1
Maciej <raniszem@gmail.com>
2026-06-01 20:46:38 +0200

    Implement player location tracking in behavior tree service

--- Source/ShooterSam/BTService_PlayerLocationIfSeen.cpp
+++ Source/ShooterSam/BTService_PlayerLocationIfSeen.cpp
@@ -3,6 +3,9 @@
 
 #include "BTService_PlayerLocationIfSeen.h"
 
+#include "ShooterAI.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
 UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
 {
 	NodeName = TEXT("Update PlayerLocation If Seen");
@@ -11,5 +14,22 @@ UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
 void UBTService_PlayerLocationIfSeen::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
-	UE_LOG(LogTemp, Log, TEXT("Service is ticking: %f"), DeltaSeconds);
+	
+	AShooterAI* OwnerController = Cast<AShooterAI>(OwnerComp.GetAIOwner());
+	AShooterSamCharacter* Player = OwnerController->PlayerCharacter;
+	UBlackboardComponent* Blackboard = OwnerController->GetBlackboardComponent();
+	
+	if (OwnerController && Player && Blackboard)
+	{
+		if (OwnerController->LineOfSightTo(Player))
+		{
+			Blackboard->SetValueAsVector(GetSelectedBlackboardKey(), Player->GetActorLocation());
+			OwnerController->SetFocus(Player);
+		}
+		else
+		{
+			Blackboard->ClearValue(GetSelectedBlackboardKey());
+			OwnerController->ClearFocus(EAIFocusPriority::Gameplay);
+		}
+	}
 }
