fa555ce05822b1f6c53641e3b9b1b35961bfdccf
Maciej <raniszem@gmail.com>
2026-06-01 21:09:48 +0200

    Add player location tracking service to behavior tree

---
+++ Source/ShooterSam/BTService_PlayerLocation.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_PlayerLocation.h"
+
+#include "ShooterAI.h"
+#include "ShooterSamCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTService_PlayerLocation::UBTService_PlayerLocation(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	NodeName = TEXT("Get Player Location");
+}
+
+void UBTService_PlayerLocation::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	AShooterAI* OwnerController = Cast<AShooterAI>(OwnerComp.GetAIOwner());
+	AShooterSamCharacter* Player = OwnerController->PlayerCharacter;
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+	
+	if (Player && Blackboard)
+	{
+		Blackboard->SetValueAsVector(GetSelectedBlackboardKey(), Player->GetActorLocation());
+	}
+}
